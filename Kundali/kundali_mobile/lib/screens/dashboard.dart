f15e8a39240aeba1a44da92b4498a995453d8a7f
aakriti82 <aakritiyadav82@gmail.com>
2025-08-03 14:24:16 +0545

    initial changes

--- Kundali/kundali_mobile/lib/screens/dashboard.dart
+++ Kundali/kundali_mobile/lib/screens/dashboard.dart
@@ -6,19 +6,59 @@ class DashboardScreen extends StatelessWidget {
 
   DashboardScreen({required this.token});
 
+  void _logout(BuildContext context) {
+    // Implement your logout logic here
+    Navigator.of(context).popUntil((route) => route.isFirst);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Dashboard")),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Create Kundali"),
-          onPressed: () => Navigator.push(
-            context,
-            MaterialPageRoute(builder: (_) => KundaliFormScreen(token: token)),
-          ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("User Name"),
+              accountEmail: Text("user@email.com"),
+              currentAccountPicture: CircleAvatar(
+                child: Icon(Icons.person),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text("Logout"),
+              onTap: () => _logout(context),
+            ),
+          ],
         ),
       ),
-    );
-  }
-}
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Text(
+              "Welcome to your Dashboard!",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(height: 24),
+            ElevatedButton.icon(
+              icon: Icon(Icons.add),
+              label: Text("Create Kundali"),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => KundaliFormScreen(token: token)),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton.icon(
+              icon: Icon(Icons.list),
+              label: Text("View Kundalis"),
+              onPressed: () {
+                // TODO: Implement navigation to Kundali list screen
+              },
+            ),
+          ],
+        ),
+      ),
